0b6ed249a4cc86cbe3c93a9a8bc5e702795ae1ee
ducanhlaminh <89836104+ducanhlaminh@users.noreply.github.com>
2022-02-11 23:16:05 +0700

    556

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var donate = document.querySelector(".donate");
 var outModal = document.querySelector(".modal-container")
 var outModal2 = document.querySelector(".skip a")
+var search = document.querySelector(".search");
 donate.onclick = function(){
     document.querySelector(".modal").style.display ='block';
 }
-outModal,outModal2.onclick = function(){
+outModal2.onclick = function(){
     document.querySelector(".modal").style.display ='none';
 }
+outModal.onclick = function(){
+    document.querySelector(".modal").style.display ='none';
+}
+search.onclick = function(){
+    document.querySelector(".input-search").style.display ='block';
+}
+
