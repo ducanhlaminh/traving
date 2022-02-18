b5509ac06655f0e55db1302268ca2b01742ea8f0
ducanhlaminh <89836104+ducanhlaminh@users.noreply.github.com>
2022-02-18 23:03:17 +0700

    123

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ var search = document.querySelector(".search");
 var start = document.querySelector(".btn-signup");
 donate.onclick = function(){
     document.querySelector(".modal").style.display ='block';
+    document.querySelector(".modal-content").style.display ='block';
+
 }
 outModal2.onclick = function(){
     document.querySelector(".modal").style.display ='none';
+    document.querySelector(".modal-content").style.display ='none';
+
 }
 
 search.onclick = function(){
@@ -15,5 +19,7 @@ search.onclick = function(){
 }
 start.onclick = function(){
     document.querySelector(".modal").style.display ='none';
+    document.querySelector(".modal-login-signup").style.display ='none';
+    alert("BẠN ĐÃ ĐĂNG KÝ THÀNH CÔNG")
 }
 
